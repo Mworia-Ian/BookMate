5efd6aa06473a7ec86dc7561e6c4c855a45b9f39
najibdevs <najibsoftwareeng@gmail.com>
2024-07-11 19:52:32 +0100

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 className="text-xl font-bold underline">Hello world!</h1>
+      <h1 className="text-xl font-bold underline">Hellooooooo!</h1>
     </div>
   );
 }
