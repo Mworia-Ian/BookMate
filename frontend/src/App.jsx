bcab62fb7b42c254aa2257c9b9e7b77eb024a748
Deborah Wasere Ndole <debbiendole@gmail.com>
2024-07-17 15:16:41 +0300

    contact page updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Favourite from "./components/pages/Favourite"
+import ContactUsPage from "./components/pages/Contact"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/favourite" element={<Favourite/>} />
+        <Route path="/contact" element={<ContactUsPage/>} />
       </Routes>
     </BrowserRouter>
   )
