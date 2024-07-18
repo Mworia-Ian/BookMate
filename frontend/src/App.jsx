3d75b32a4240364f4371bc03ccae753f8cb0885e
ianmmworia <ianmworia156@gmail.com>
2024-07-19 01:03:50 +0300

    configured the new routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import SignUp from "./components/SignUp";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Home from './components/Home';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<SignUp />}/>
+        <Route path="/signup" element={<Signup />} />
+        {/* <Route path="/home" element={<Home />} /> */}
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
