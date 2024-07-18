b3f7887b3e82695a5b40c5e6ea961f5c6a0c41ea
ianmmworia <ianmworia156@gmail.com>
2024-07-19 00:33:23 +0300

    created new migrations

--- backend/config.py
+++ backend/config.py
@@ -8,10 +8,9 @@ app = Flask(__name__)
 
 CORS(app)
 
-# Update the database URI to use PostgreSQL
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://username:password@localhost/bookmate"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:123456@localhost/bookmate"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["JWT_SECRET_KEY"] = "your-secret-key"  # Change this to a secure secret key
+app.config["JWT_SECRET_KEY"] = "your-secret-key"  
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
--- backend/models.py
+++ backend/models.py
@@ -25,7 +25,7 @@ class User(db.Model, UserMixin):
     first_name = db.Column(db.String(80), nullable=False)
     last_name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
