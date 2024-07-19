fa6e3a766da1a377aaec9a23916bfe22905a22e6
ianmmworia <ianmworia156@gmail.com>
2024-07-19 09:35:01 +0300

    added a home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/SignUp';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        {/* <Route path="/home" element={<Home />} /> */}
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </Router>
   );
