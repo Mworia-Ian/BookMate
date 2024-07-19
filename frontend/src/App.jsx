65d21864799667acc0ef9110d3a64f31acde9661
Victor Nyingi Muchiri <nyingimuchiriv@gmail.com>
2024-07-19 17:21:45 +0300

    done changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/SignUp';
 
-
 function App() {
   return (
     <Router>
